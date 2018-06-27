63065875aefdf4c862d235b5d984731a13fb10b0
jadedResearcher <gtg287y@gmail.com>
2018-06-27 01:41:03 -0400

    if shit rotated i could turn debugging thingies off and call this a sucess

--- web/main.dart
+++ web/main.dart
@@ -4,12 +4,20 @@ import 'dart:math' as Math;
 import 'demo.dart';
 import 'package:box2d/box2d.dart';
 import 'package:LoaderLib/Loader.dart';
+import "package:DollLibCorrect/DollRenderer.dart";
+
+int type = 113;
+
+List<CanvasElement> birbs;
 
 Future<Null>main() async {
+  if(getParameterByName("type", null) != null) {
+   type = int.parse(getParameterByName("type", null));
+  }
   querySelector('#output').text = 'Your Dart app is running.';
   //65 x 65
-  ImageElement birb = await Loader.getResource("images/pigeon.png");
-  PigeonDemo pigeon = new PigeonDemo("HELLOWORLD", birb);
+  birbs = await initBirbs();
+  PigeonDemo pigeon = new PigeonDemo("HELLOWORLD", birbs);
 
 
 
@@ -25,10 +33,57 @@ Future<Null>main() async {
   });
 }
 
+//http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+//simulatedParamsGlobalVar is the simulated global vars.
+String getParameterByName(String name, [String url]) {
+  Uri uri = Uri.base;
+  String tmp = null;
+  if (url != null) {
+    uri = Uri.parse(url);
+    // //print("uri is $uri");
+    String tmp = (uri.queryParameters[name]); //doesn't need decoded, guess it was auto decoded with the parse?
+    if(tmp != null) return tmp;
+  } else {
+    ////print("uri is $uri");
+    String tmp = (uri.queryParameters[name]);
+    if (tmp != null) tmp = Uri.decodeComponent(tmp);
+    if(tmp != null) return tmp;
+  }
+  ////print("gonna check simulated params");
+
+
+  return tmp;
+}
+
+Future<List<CanvasElement>> initBirbs() async {
+  List<CanvasElement> ret = new List<CanvasElement>();
+  DivElement stats = new DivElement()..text = "Loading Dolls of type $type";
+  querySelector("#output").append(stats);
+  //don't spend too long doing this
+  DateTime startTime = new DateTime.now();
+  for(int i = 0; i<13; i++) {
+    Doll doll = Doll.randomDollOfType(type);
+    CanvasElement canvas = new CanvasElement(width:doll.width, height:doll.height);
+    await DollRenderer.drawDoll(canvas,doll);
+    CanvasElement finalCanvas = new CanvasElement(width:64, height:65);
+    await Renderer.cropToVisible(canvas);
+    await Renderer.drawToFitCentered(finalCanvas, canvas);
+    ret.add(finalCanvas);
+    print("drew $doll");
+    stats.text = "Loaded $i dolls of type $type";
+    DateTime currentTime = new DateTime.now();
+    Duration diff = currentTime.difference(startTime);
+    if(diff.inMilliseconds >13000) {
+      break;
+    }
+  }
+  return ret;
+}
+
 
 class PigeonDemo extends Demo {
-  ImageElement birb;
-  PigeonDemo(String name, ImageElement this.birb) : super(name);
+  List<CanvasElement> birbs;
+  PigeonDemo(String name, List<CanvasElement> this.birbs) : super(name);
 
 
   void initialize() {
@@ -43,14 +98,23 @@ class PigeonDemo extends Demo {
     // ignore: conflicting_dart_import
     for(Body b in bodies) {
       if(b.getType() == BodyType.DYNAMIC) {
+        CanvasElement birb = getRandomBirb(bodies.indexOf(b));
         //looks like 0,0 is in the center, and y is inverted. viewport.scale handles coordinate conversion
         num x = b.position.x * viewport.scale + canvas.width / 2 -
             birb.width / 2;
         num y = canvas.height - (b.position.y * viewport.scale) -
             canvas.height / 2 - birb.height / 2;
+        canvas.context2D.save();
+        canvas.context2D.rotate(b.getAngle());
+        canvas.context2D.restore();
         canvas.context2D.drawImage(birb, x, y);
       }
     }
+  }
+
+  CanvasElement getRandomBirb(int seed) {
+    Random rand = new Random(seed);
+    return rand.pickFrom(birbs);
   }
 
   void _createGround() {
